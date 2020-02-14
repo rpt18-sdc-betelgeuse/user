96fc49e92f021d0f4bb66adc2fabb8e9f71ee267
Shain-Carrasco <shaincarrasco@gmail.com>
2020-02-14 12:05:30 -0800

    making changes in database index.js wanted to not break everything

--- database/index.js
+++ database/index.js
@@ -1,44 +1,60 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/soundcloud', {
-  useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false,
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Shain:G1mm3m0ng0@cluster0-xgt6f.mongodb.net/test1?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+
+  if (err) {
+    console.log(err);
+  } else { console.log('connected'); }
+  const collection = client.db("test1").collection("devices");
+  // perform actions on the collection object
+  //client.close();
 });
 
-const userSchema = mongoose.Schema({
-  id: Number,
-  handle: String,
-  name: String,
-  image_url: String,
-  track_count: Number,
-  follower_count: Number,
-  join_date: Date,
-});
 
-const User = mongoose.model('User', userSchema);
 
-const getUserById = (ID, callback) => {
-  User.findOne({ id: ID }).exec(callback);
-};
+// // mongoose.connect('mongodb://localhost/soundcloud', {
+// //   useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false,
+// // });
 
-const getUserByName = (username, callback) => {
-  User.findOne({ name: username }).exec(callback);
-};
+// const userSchema = mongoose.Schema({
+//   id: Number,
+//   handle: String,
+//   name: String,
+//   image_url: String,
+//   track_count: Number,
+//   follower_count: Number,
+//   join_date: Date,
+// });
 
-const getAllUsers = (callback) => {
-  User.find({}).exec(callback);
-};
+// const User = mongoose.model('User', userSchema);
 
-const decrementFollowers = (username, callback) => {
-  User.findOneAndUpdate({ name: username }, { $inc: { follower_count: -1 } }).exec(callback);
-};
+// const getUserById = (ID, callback) => {
+//   User.findOne({ id: ID }).exec(callback);
+// };
 
-const incrementFollowers = (username, callback) => {
-  User.findOneAndUpdate({ name: username }, { $inc: { follower_count: 1 } }).exec(callback);
-};
+// const getUserByName = (username, callback) => {
+//   User.findOne({ name: username }).exec(callback);
+// };
 
-module.exports.getUserById = getUserById;
-module.exports.decrementFollowers = decrementFollowers;
-module.exports.incrementFollowers = incrementFollowers;
-module.exports.getAllUsers = getAllUsers;
-module.exports.getUserByName = getUserByName;
-module.exports.User = User;
+// const getAllUsers = (callback) => {
+//   User.find({}).exec(callback);
+// };
+
+// const decrementFollowers = (username, callback) => {
+//   User.findOneAndUpdate({ name: username }, { $inc: { follower_count: -1 } }).exec(callback);
+// };
+
+// const incrementFollowers = (username, callback) => {
+//   User.findOneAndUpdate({ name: username }, { $inc: { follower_count: 1 } }).exec(callback);
+// };
+
+// module.exports.getUserById = getUserById;
+// module.exports.decrementFollowers = decrementFollowers;
+// module.exports.incrementFollowers = incrementFollowers;
+// module.exports.getAllUsers = getAllUsers;
+// module.exports.getUserByName = getUserByName;
+// module.exports.User = User;
